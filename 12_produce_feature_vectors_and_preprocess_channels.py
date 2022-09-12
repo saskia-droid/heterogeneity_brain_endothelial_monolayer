915bf71f8938fff07e8382661d9c7bab172768fa
Saskia <saskia.perretgentil@gmail.com>
2022-09-12 14:14:28 +0200

    first commit

---
+++ 12_produce_feature_vectors_and_preprocess_channels.py
@@ -0,0 +1,71 @@
+# libraries
+
+import os
+import tensorflow as tf
+import pickle
+from PIL import Image
+import numpy as np
+import cv2
+from skimage.morphology import disk
+from skimage.filters import median
+
+if __name__ == '__main__':
+
+    # defining paths
+    path_to_new_frames = '/Volumes/tki/EngelhardtGroup/Saskia/data/exported_frames'
+    main_dir_path = os.path.join(path_to_new_frames, "flow/single_z")
+
+    path_to_preprocess_channel_images = '/Users/saskia/unibe19/master_thesis/TKI_project/data/preprocess_channel_images/flow/single_z'
+
+    path_to_dataset = '/Users/saskia/unibe19/master_thesis/TKI_project/fluo_datasets/'
+
+    # upload the model
+    #models_path = '/Users/saskia/unibe19/master_thesis/TKI_project/UBELIX/models/'
+    #model = tf.keras.models.load_model(os.path.join(models_path, '78_3c_l2Normalize_lrelu_bestEpoch.h5'))
+
+    # extract trained layers that we need
+    #model.layers.pop(0)
+    #new_model = tf.keras.Sequential()
+    #new_model.add(tf.keras.Input(shape=(None, None, 1)))  # change input dim
+    #for layer in model.layers[1:-2]:  # -2 -> third last layer as output
+    #    new_model.add(layer)
+
+    #c0 = []
+    c3 = []
+
+    for i in range(18):
+        ### c0
+
+        #img = Image.open(os.path.join(main_dir_path, str(i), 'c000_t009_z000.png'))
+
+        # convert image to numpy array
+        #data = np.asarray(img)
+
+        # get feature map
+        #feature_map = new_model.predict(data[None, :, :, None])
+
+        #fv = feature_map.reshape(-1, feature_map.shape[-1])
+        #c0.append(fv)
+
+        ### c3
+
+        img = Image.open(os.path.join(main_dir_path, str(i), 'c003_t009_z000.png'))
+
+        # convert image to numpy array
+        data = np.asarray(img)
+
+        img32 = cv2.resize(data, None, fx=1 / 32, fy=1 / 32, interpolation = cv2.INTER_AREA)
+        img_medFilt = median(img32, disk(3))
+        img_V = img_medFilt.reshape(-1, 1)
+        c3.append(img_V)
+
+    # transform the list to a numpy array
+    #c0 = np.array(c0)
+    c3 = np.array(c3)
+
+    #c0 = c0.reshape(-1, 64)
+    c3 = c3.reshape(-1, 1)
+
+    # pickle everything...
+    #pickle.dump(c0, open(os.path.join(path_to_dataset, 'c0.pckl'), 'wb'))
+    pickle.dump(c3, open(os.path.join(path_to_dataset, 'c3_preprocessA.pckl'), 'wb'))
